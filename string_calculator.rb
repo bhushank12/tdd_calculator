61476db31a7d29a014d5afda60210be681278900
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-03-18 13:18:11 +0530

    Handle negative number exception for add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,22 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
+    raise_negative_number(numbers) if negatives(numbers).any?
 
     digits(numbers).sum
   end
 
   private
 
-  def self.digits(numbers)
+  def self.negatives(numbers)
+    digits(numbers).select { |digit| digit.negative? }
+  end
+
+	def self.raise_negative_number(numbers)
+    raise "negative numbers not allowed #{negatives(numbers).join(', ')}"
+  end
+
+	def self.digits(numbers)
     if custom_delimiter?(numbers)
       delimiter = numbers[2]
       numbers.split("\n").last.split(delimiter).map(&:to_i)
@@ -16,7 +25,7 @@ class StringCalculator
     end
   end
 
-  def self.custom_delimiter?(numbers)
+	def self.custom_delimiter?(numbers)
     numbers.start_with?("//")
   end
 end
