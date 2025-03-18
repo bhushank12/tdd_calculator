82d631f2ed7443291b9791c65e914aa130e6e8ec
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-03-18 13:02:18 +0530

    Added support for addition of numbers using custom delimiter in add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,21 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/[\n,]/).map(&:to_i).sum
+    digits(numbers).sum
+  end
+
+  private
+
+  def self.digits(numbers)
+    if custom_delimiter?(numbers)
+      delimiter = numbers[2]
+      numbers.split("\n").last.split(delimiter).map(&:to_i)
+    else
+      numbers.split(/[\n,]/).map(&:to_i)
+    end
+  end
+
+  def self.custom_delimiter?(numbers)
+    numbers.start_with?("//")
   end
 end
