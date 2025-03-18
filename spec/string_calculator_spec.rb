526e2097642f3ad22016ba268259248a2169fab0
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-03-18 12:16:44 +0530

    Added test cases when argument having two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it "returns the number itself if only one number is provided" do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two numbers" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
   end
 end
